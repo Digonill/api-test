88bc1a06c1a651d531b59b7059f13de9ce9a9005
Rodrigo Oliveira <digonill@gmail.com>
2021-10-22 22:35:20 -0300

    refact api and fix bugs

--- src/environment/environment.py
+++ src/environment/environment.py
@@ -3,10 +3,8 @@ import os
 env = os.environ.get("PYTHON_ENV", "development")
 
 all_environments = {
-    "development": { "port": 5000, "debug": True, "swagger-url": "/doc" },
-    "production": { "port": 8080, "debug": False, "swagger-url": "/doc"  }
+    "development": {"port": 5000, "debug": True, "swagger-url": "/doc", "prefix": "/api"},
+    "production": {"port": 8080, "debug": False, "swagger-url": "/doc", "prefix": "/api"}
 }
-
-
 
 environment_config = all_environments[env]
